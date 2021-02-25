801324aac18e96f895c9d1582ea0c936d8f5d563
aidanchill <aidanchill@gmail.com>
2021-02-25 13:36:04 -0500

    populating Clients table from db

--- route.js
+++ route.js
@@ -18,7 +18,9 @@ app.use(bodyParser.json());
 
 const getAllCerts='SELECT * FROM Certifications';
 const getAllTrainers = 'SELECT * FROM Trainers';
+const getAllClients = 'SELECT * FROM Clients INNER JOIN Trainers ON Clients.TrainerID = Trainers.TrainerID INNER JOIN ClientPlans ON Clients.ClientID = ClientPlans.ClientID INNER JOIN ExercisePlans ON ClientPlans.ExerciseID = ExercisePlans.ExerciseID';
 const getTrainersWithCerts = 'SELECT Trainers.TrainerFN, Trainers.TrainerLN, Trainers.TrainerEmail, Certifications.CertTitle FROM Trainers INNER JOIN TrainerCerts ON Trainers.TrainerID = TrainerCerts.TrainerID INNER JOIN Certifications ON TrainerCerts.CertID = Certifications.CertID';
+
 
 app.get('/',function(req,res){
   res.render('index', {});
@@ -37,15 +39,24 @@ app.get('/certs',function(req,res, next){                    // render certs pag
       next(err);
       return;
     }
-    context.results = JSON.stringify(rows);
+    context.results = rows;
     console.log(context);
     res.render('certs', context)
   });
 });
 
 
-app.get('/clients',function(req,res){                   // render clients page when you visit certs url
-  res.render('clients', {});
+app.get('/clients',function(req,res){ 
+  var context = {};
+  mysql.pool.query(getAllClients, function(err, rows, fields) {
+    if (err) {
+      next(err);
+      return;
+    }
+    context.results = rows;
+    console.log(context);
+    res.render('clients', context);
+  });              // render clients page when you visit certs url
 });
 
 app.get('/mngclients',function(req,res){                // render manage clients page when you visit mngclients url
